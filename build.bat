b4dab8765a9bc9b9d1083c0996c915e757563f1e
deadc0de128 <95382180+deadc0de128@users.noreply.github.com>
2023-08-03 12:39:42 +0100

    Remove VS files, move env vars to presets file

--- build.bat
+++ build.bat
@@ -11,9 +11,6 @@ echo Build type = %PRESET%
 
 rmdir/S/Q %BUILD_DIR%
 
-set SDL2_DIR=..\..\SDL2-2.28.2
-set SDL2_IMAGE_DIR=..\..\SDL2_image-2.6.3
-
 cmake -S . --preset %PRESET% 
 cmake --build %BUILD_DIR% --verbose
 
